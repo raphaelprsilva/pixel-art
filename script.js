4fce21ad9c0c121c9327b0cac2f4f461cd80a780
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-04-10 13:21:35 -0300

    Create function that create class selected

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function setFirstElement() {
-  let paletteList = document.querySelector('#color-palette');
-  let firstItem = paletteList.firstElementChild;
+  const paletteList = document.querySelector('#color-palette');
+  const firstItem = paletteList.firstElementChild;
   firstItem.className = 'color selected';
 }
 
 setFirstElement();
+
+function selectColor() {
+  let colorPalette = document.querySelector('#color-palette');
+  let childColorPalette = colorPalette.children;
+  
+  colorPalette.addEventListener('click', function(event) {
+    for (let index = 0; index < childColorPalette.length; index += 1) {
+      childColorPalette[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  });
+}
+
+selectColor();
