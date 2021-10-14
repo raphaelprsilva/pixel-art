bb9272ff4e40ee9d31aae91fd13559a13d4b72ef
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-14 10:42:14 -0300

    Refactoring code - Add option to change pixel color on click

---
+++ script.js
@@ -0,0 +1,31 @@
+const paletteList = document.querySelector('[data-js="color-palette"]');
+const pixelsBoard = document.querySelector('[data-js="pixel-board"]');
+
+const colorPalette = 'palette__list';
+const selectedColorValue = 'color selected';
+
+const setColorClass = (paletteColors) => {
+  for (let index = 0; index < paletteColors.length; index += 1) {
+    const currentColor = paletteColors[index];
+    currentColor.className = 'color';
+  }
+};
+
+paletteList.addEventListener('click', (event) => {
+  const clickedTarget = event.target;
+
+  if (clickedTarget.parentNode.className.includes(colorPalette)) {
+    const paletteColors = Array.from(paletteList.children);
+
+    setColorClass(paletteColors);
+    clickedTarget.className = selectedColorValue;
+  }
+});
+
+pixelsBoard.addEventListener('click', (event) => {
+  const clickedElement = event.target;
+  const selectedColor = document.querySelector('.selected');
+  const selectedBackgroundColor = selectedColor.style.backgroundColor;
+
+  clickedElement.style.backgroundColor = selectedBackgroundColor;
+});
